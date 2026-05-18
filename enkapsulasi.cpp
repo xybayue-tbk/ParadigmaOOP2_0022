0de2f1fe2dbd23c07304b092828bc47dd1c349e6
xybayue-tbk <xybayue@gmail.com>
2026-05-18 13:19:04 +0700

    menambahkan class remotlampu dan di dalamnya

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+
+class remoteLampu {
+private:
+    string saklarNo[10];
+
+public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
