8da841c1057d52977c69216ec6986d9a0164998c
xybayue-tbk <xybayue@gmail.com>
2026-05-18 13:05:47 +0700

    menambahkan isi main progam

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -25,4 +25,18 @@ public:
 };
 
 int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+
+    obyek = &b;
+    obyek->pesan();
+
+    // a.seseorang::pesan();
+    // b.seseorang::pesan();
+
+    return 0;
 }
